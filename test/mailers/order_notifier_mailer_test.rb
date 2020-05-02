09db792742a529df47a9492e90b1bc0f48dd8c5b
MarrMika <slobodjanmarija1@gmail.com>
2020-05-02 18:28:41 +0300

    LAB-12 Add mail sender when user completed ordering

---
+++ test/mailers/order_notifier_mailer_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class OrderNotifierMailerTest < ActionMailer::TestCase
+  test "received" do
+    mail = OrderNotifierMailer.received(orders(:one))
+    assert_equal "Подтверждение заказа в Pragmatic Store", mail.subject
+    assert_equal ["dave@example.org"], mail.to
+    assert_equal ["depot@example.com"], mail.from
+  end
+
+  test "shipped" do
+    mail = OrderNotifierMailer.shipped(orders(:one))
+    assert_equal "Заказ из Pragmatic Store отправлен", mail.subject
+    assert_equal ["dave@example.org"], mail.to
+    assert_equal ["depot@example.com"], mail.from
+  end
+
+end
